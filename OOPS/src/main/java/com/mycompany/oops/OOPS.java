93eba2e04e6598831546577cc5d48f51af8a2344
VedUplenchwar <118630716+VedUplenchwar@users.noreply.github.com>
2023-04-08 15:37:53 +0400

    Add files via upload

---
+++ OOPS/src/main/java/com/mycompany/oops/OOPS.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.oops;
+
+/**
+ *
+ * @author vedup
+ */
+public class OOPS {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
